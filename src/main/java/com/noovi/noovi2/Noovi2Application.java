604564c1cba5bfab0fc54a4a378db51c83ae2a26
Felipe Corrêa <ffelipe.corea@gmail.com>
2022-08-08 01:24:03 -0300

    Criando o projeto

---
+++ src/main/java/com/noovi/noovi2/Noovi2Application.java
@@ -0,0 +1,13 @@
+package com.noovi.noovi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Noovi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Noovi2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/noovi/noovi2/Noovi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noovi.noovi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Noovi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
